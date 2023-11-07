5dea95267325e9950eca8806ca140b5bba21a229
Christian Gudrian <cgu@ivu.de>
2023-11-07 16:05:26 +0100

    wip

--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,13 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+import { contextBridge, ipcRenderer } from 'electron'
+
+const exposedApi = {
+    onUSBChanged: (callback: (devices: { [key: number]: string }) => void) => ipcRenderer.on('usb-changed', (_, devices) => callback(devices)),
+    onUpdateCounter: (callback: (delta: number) => void) => ipcRenderer.on('update-counter', (_, delta) => callback(delta))
+}
+
+contextBridge.exposeInMainWorld('api', exposedApi)
+
+export type BackendApi = typeof exposedApi
